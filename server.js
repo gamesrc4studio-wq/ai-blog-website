0948a0db4c584ba3bdda46f511acabc391d9be98
BUDDIES07 <154003939+BUDDIES07@users.noreply.github.com>
2026-05-26 09:59:51 +0530

    fixed secrets

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const fs = require("fs").promises;
 const path = require("path");
 const marked = require("marked");
-
+require("dotenv").config()
 const app = express();
 
 
